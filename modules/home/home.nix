cf6d31603b08a4d7844cfe7c1fa6ffc07c06d73d
Erik Jagyugya <jagyugyaerik@gmail.com>
2026-04-07 00:55:56 +0200

    Enable zsh autosuggestions and syntax highlighting
    Added home-manager options to enable:
    - autosuggestion.enable = true (shows command suggestions as you type)
    - syntaxHighlighting.enable = true (highlights valid/invalid commands)
    - enableCompletion = true (enables tab completion)
    
    These features are now properly integrated with oh-my-zsh.

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -97,6 +97,9 @@
 
   programs.zsh = {
     enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
     oh-my-zsh = {
       enable = true;
       theme = "robbyrussell";
